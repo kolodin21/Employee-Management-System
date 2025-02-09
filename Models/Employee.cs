5f9040f138a1d9567df1cf75a889450d41fc637e
kolodin21 <kolodin21@yandex.ru>
2025-02-09 21:17:18 +0300

    Создал классы

---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; } // Может быть null
+    }
+
+    public class Employee 
+    {
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+    public class EmployeeDto
+    {
+
+
+
+    }
+}
