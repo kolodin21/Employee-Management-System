e5253c0b784cf2fb49fd115c2f6f0c358c928a4d
Astakhov Artem <astahov-lr@mail.ru>
2025-02-09 23:09:05 +0500

    Добавил классы для работы с БД

---
+++ Server.DAL/Repository/LeaveRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Server.DAL.Repository;
+
+public class LeaveRepository
+{
+    public async Task AddLeaveAsync(Leave leave)
+    {
+        
+    }
+
+    public async Task<IEnumerable<LeaveDto>> GetLeavesByEmployeeAsync(int employeeId)
+    {
+        return null;
+    }
+    
+    public async Task<IEnumerable<LeaveDto>> GetLeavesByDateRangeAsync(DateTime beginDate, DateTime endDate)
+    {
+        return null;
+    }
+
+    public async Task<IEnumerable<LeaveDto>> GetLeaveBalanceAsync(int employeeId)
+    {
+        return null;
+    }
+    
+    public async Task CancelLeaveAsync(int leaveId)
+    {
+        
+    }
+    
+}
