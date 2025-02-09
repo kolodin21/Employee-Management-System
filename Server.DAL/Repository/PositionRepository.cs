e04d78386aa7ffecc85007c9225bba5228479f92
Astakhov Artem <astahov-lr@mail.ru>
2025-02-09 23:09:05 +0500

    Добавил классы для работы с БД

---
+++ Server.DAL/Repository/PositionRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Server.DAL.Repository;
+
+public class PositionRepository
+{
+    public async Task AddPositionAsync(string name)
+    {
+        
+    }
+
+    public async Task<IEnumerable<PositionDto>> GetPositionsAsync()
+    {
+        return null;
+    }
+    
+    public async Task UpdatePositionAsync(int id, string name)
+    {
+        
+    }
+    
+    public async Task DeletePositionAsync(int positionId)
+    {
+        
+    }
+}
---
+++ Server.DAL/Repository/ReportSearchRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Server.DAL.Repository;
+
+public class ReportSearchRepository
+{
+    
+    public async Task<IEnumerable<ReportDto>> GenerateEmployeeReportAsync(int employeeId)
+    {
+        return null;
+    }
+    
+    public async Task<IEnumerable<ReportDto>> GenerateDepartmentReportAsync(int departmentId)
+    {
+        return null;
+    }
+    
+    public async Task<EmployeeDto> GenerateLeaveStatisticsAsync(DateTime start, DateTime end)
+    {
+        return null;
+    }
+
+    public async Task<EmployeeDto> SearchEmployeesAsync(string lastName, string firstName)
+    {
+        return null;
+    }
+    
+}
