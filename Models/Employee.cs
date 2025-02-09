4bf3ea86828b0d7a140623035bcfb4057a727326
kolodin21 <kolodin21@yandex.ru>
2025-02-09 22:11:48 +0300

    Создал классы ,Dto

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -2,24 +2,27 @@
 {
     public class Person
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public int Id { get; set; }             // Id сотрудника
+        public string Name { get; set; }        // Имя
+        public string Surname { get; set; }     // Фамилия
         public string? Patronymic { get; set; } // Может быть null
     }
 
-    public class Employee 
+    public class Employee : Person
     {
-        public string Department { get; set; }
-        public string Position { get; set; }
-        public DateTime HireDate { get; set; }
-        public bool IsActive { get; set; }
+        public int DepartmentId { get; set; } 
+        public int PositionId { get; set; }
+        public DateTime HireDate { get; set; } // Дата устройства на работу
+        public bool IsActive { get; set; }     // статус сотрудника (активный/уволенный).
     }
 
-    public class EmployeeDto
+
+    public class EmployeeDto : Person
     {
-
-
-
+        public int DepartmentId { get; set; }
+        public string Department { get; set; } // Название департамента
+        public int PositionId { get; set; }
+        public string Position { get; set; }   // Название должности
+        public DateTime HireDate { get; set; } // Дата устройства на работу
     }
 }
