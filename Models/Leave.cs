8c1c43f1b9c8473e661c15f177e9b5eb88573d8a
kolodin21 <kolodin21@yandex.ru>
2025-02-09 21:17:18 +0300

    Создал классы

---
+++ Models/Leave.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+    //Тип отсутствия
+    public enum LeaveType
+    {
+        Vacation = 1, //Отпуск
+        Medical,      //Больничный
+        Dismissed     //Уволен
+    }
+
+    public class Leave 
+    {
+        public int Id { get; set; }             // Id записи
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? Patronymic { get; set; } // Может быть null
+        public LeaveType LeaveType { get; set; } 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
