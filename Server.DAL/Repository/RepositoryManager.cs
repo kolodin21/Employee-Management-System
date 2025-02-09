1fe3544f698f1f59ae8ad9827c9799de99619fc2
Astakhov Artem <astahov-lr@mail.ru>
2025-02-09 23:09:05 +0500

    Добавил классы для работы с БД

--- Server.DAL/Repository/RepositoryManager.cs
+++ Server.DAL/Repository/RepositoryManager.cs
@@ -1,20 +1,6 @@
 ﻿namespace Server.DAL.Repository
 {
-    public class RepositoryManager
+    public abstract class RepositoryManager
     {
-        public DepartmentRepository DepartmentRepository { get; }
-        public EmployeeRepository EmployeeRepository { get; }
-        public LeaveRepository LeaveRepository { get; }
-        public PositionRepository PositionRepository { get; }
-        public ReportSearchRepository ReportSearchRepository { get; }
-
-        public RepositoryManager()
-        {
-            DepartmentRepository = new DepartmentRepository();
-            EmployeeRepository = new EmployeeRepository();
-            LeaveRepository = new LeaveRepository();
-            PositionRepository = new PositionRepository();
-            ReportSearchRepository = new ReportSearchRepository();
-        }
     }
 }
