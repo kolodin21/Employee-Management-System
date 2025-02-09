e02bc2904afea5dfcaeec8093ed3c805f7d43c3c
kolodin21 <kolodin21@yandex.ru>
2025-02-09 22:21:40 +0300

    Добавил инициализацию классов в RepositoryManager Чуть изменил возвращаемые классы в методах

--- Server.DAL/Repository/RepositoryManager.cs
+++ Server.DAL/Repository/RepositoryManager.cs
@@ -2,5 +2,19 @@
 {
     public class RepositoryManager
     {
+        public DepartmentRepository DepartmentRepository { get; }
+        public EmployeeRepository EmployeeRepository { get; }
+        public LeaveRepository LeaveRepository { get; }
+        public PositionRepository PositionRepository { get; }
+        public ReportSearchRepository ReportSearchRepository { get; }
+
+        public RepositoryManager()
+        {
+            DepartmentRepository = new DepartmentRepository();
+            EmployeeRepository = new EmployeeRepository();
+            LeaveRepository = new LeaveRepository();
+            PositionRepository = new PositionRepository();
+            ReportSearchRepository = new ReportSearchRepository();
+        }
     }
 }
