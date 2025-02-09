d70f4dc9522fcbf79a22bdb6ad29126266dd35a7
kolodin21 <kolodin21@yandex.ru>
2025-02-09 21:37:45 +0300

    Изменил способ подключения к базе данных

---
+++ Server.DAL/Repository/DatabaseConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Server.DAL.Repository;
+
+public class DatabaseConfig 
+{
+    private static readonly IConfiguration Configuration;
+
+    static DatabaseConfig()
+    {
+        Configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile(@"Configuration\appsetting.json", optional: false, reloadOnChange: true)
+            .Build();
+    }
+
+    public static string ConnectionString => Configuration["ConnectionString:DefaultConnection"]
+                                             ?? throw new InvalidOperationException(
+                                                 "ConnectionStrings not found in configuration.");
+
+    public static string DatabaseSchema => Configuration["TableSchema"] ??
+                                           throw new InvalidOperationException(
+                                               "ConnectionStrings not found in configuration.");
+}
