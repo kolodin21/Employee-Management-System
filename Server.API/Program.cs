536977cfc9d746b41935dd7737a3836731b56ce2
Dima <derferth@yandex.ru>
2025-02-13 09:03:08 +0500

    исправление не та ветвь

--- Server.API/Program.cs
+++ Server.API/Program.cs
@@ -10,16 +10,4 @@ var Logger = LogManager.GetCurrentClassLogger();
 
 Logger.Info("Приложение запущено");
 
-
-#region EmployeeRepository
-app.MapGet("/employees", async () => await EmployeeService.GetEmployeesAsync());
-app.MapGet("/employee/{id}", async (int id) => await GetEmployeeByIdAsync(int id));
-app.MapGet("/department/{id}/employees", async (int departmentId) => await GetEmployeesByDepartmentAsync(int departmentId);
-
-app.MapPost("/employees", async (Employees emp) => await AddEmployeeAsync(Employees emp));//добавить модель сотрудника
-app.MapPut("/employees", async (Employees emp) => await UpdateEmployeeAsync(DTO emp)); //добавить DTO сотрудника
-app.MapDelete("/employee/{id}", async (int id) => await DeleteEmployeeAsync(int id));
-#endregion
-
-
 app.Run();
