be924a3404d6833dfc757c7fa3e405d695495573
kolodin21 <kolodin21@yandex.ru>
2025-02-13 17:03:22 +0300

    Удлил не нужные using

--- Server.BL/DepartmentService.cs
+++ Server.BL/DepartmentService.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Models;
-using Server.DAL.Repository;
-
-namespace Server.BL
+﻿namespace Server.BL
 {
     public class DepartmentService: ServiceBase
     {
